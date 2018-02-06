5641d9b8bb38bcfca64eed245c61e349a4cf24f2
TAxford <t.axford2@yahoo.com>
2018-02-06 13:37:01 -0500

    Initial commit

---
+++ ApplianceAbstractPrcatice/Appliance.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplianceAbstractPrcatice
+{
+    public abstract class Appliance
+    {
+        //field
+        private bool isElectric;
+
+        //Propertie
+        public bool IsElectric
+        {
+            get { return this.isElectric; }
+            set { this.isElectric = value; }
+        }
+
+        //Constructor - NONE
+
+        //Methods
+        public abstract void Work();
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
